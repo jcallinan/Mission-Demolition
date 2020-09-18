38a6b9bf9953865a382762d1b625a78735529ebb
jcallinan <jeremy.callinan@gmail.com>
2020-09-17 20:23:37 -0400

    After LERP

---
+++ Mission-Demolition/Assets/FollowCam.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public float easing = 0.05f;
+    static public GameObject POI;
+    [Header("Set Dynamically")]
+    public float camZ; // the desired pos. of the camera
+
+    void Awake() {
+        camZ = this.transform.position.z;
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate() {
+        // run at max 50 fps
+        if (POI == null) return;
+        // get the position of the POI
+        Vector3 destination = POI.transform.position;
+        destination = Vector3.Lerp(transform.position, destination, easing);
+        //force dest.z to be camz to keep the camera far enough away
+        destination.z = camZ;
+        transform.position = destination;
+
+    }
+}
--- Mission-Demolition/Assets/Slingshot.cs
+++ Mission-Demolition/Assets/Slingshot.cs
@@ -55,6 +55,7 @@ public class Slingshot : MonoBehaviour
             aimingMode = false;
             projectileRigidbody.isKinematic = false;
             projectileRigidbody.velocity = - mouseDelta * velocityMult;
+            FollowCam.POI = projectile;
             projectile = null;
         }
 
