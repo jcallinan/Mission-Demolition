0e5035ae3f9c17c86b8421e97bc8b78ce4eba7f1
Jeremy Callinan <jeremy.callinan@gmail.com>
2020-09-24 19:48:44 -0400

    After Cloud crafter

--- Mission-Demolition/Assets/Cloud.cs
+++ Mission-Demolition/Assets/Cloud.cs
@@ -56,10 +56,10 @@ public class Cloud : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Restart();
-        }
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    Restart();
+        //}
     }
 
     void Restart()
